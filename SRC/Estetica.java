be82a55c653eb3e07a4ede2cd314d3ff93bc8a82
Jesús Alberto Reyes Gutiérrez <alberto.r@ciencias.unam.mx>
2022-03-21 23:08:42 -0600

    Agrega los métodos básicos de Estética.

---
+++ SRC/Estetica.java
@@ -0,0 +1,123 @@
+/**
+ * Clase que representa una estética veterinaria Little Friend.
+ */
+public class Estetica {
+    private int clave;
+    private String nombre;
+    private Direccion direccion;
+    private long telefono;
+    private String horario;
+    private short noConsultorios;
+
+    public Estetica(int clave, String nombre, Direccion direccion,
+                long telefono, String horario, short noConsultorios) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.horario = horario;
+        this.noConsultorios = noConsultorios;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public int getClave() {
+        return clave;
+    }
+
+    /**
+     * 
+     * @param clave
+     */
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * 
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * 
+     * @param direccion
+     */
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public long getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * 
+     * @param telefono
+     */
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getHorario() {
+        return horario;
+    }
+
+    /**
+     * 
+     * @param horario
+     */
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public short getNoConsultorios() {
+        return noConsultorios;
+    }
+
+    /**
+     * 
+     * @param noConsultorios
+     */
+    public void setNoConsultorios(short noConsultorios) {
+        this.noConsultorios = noConsultorios;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
