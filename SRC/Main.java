a526597791b3ea6811f4a96cc47745f5e332578d
Jesús Alberto Reyes Gutiérrez <alberto.r@ciencias.unam.mx>
2022-03-22 22:38:28 -0600

    Cambia nombre a la clase Main.

---
+++ SRC/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Clase Main que sirve de ejemplo para lo que van a entregar en la practica 2
+ * @author Ricardo Badillo Macias
+ * @version 16/03/2022
+ */
+public class Main {
+
+    
+}
